8b936f5a9b3bd75537bed2383494a02ea66012ae
Ilya Ivanov <static.ila@gmail.com>
2025-11-21 17:27:13 +0200

    Build with v4

--- build.bat
+++ build.bat
@@ -1,17 +1,17 @@
 @echo off
 
-set libs=-lkernel32 -luser32 -lgdi32.lib -ldwmapi.lib  -lwinmm.lib 
+set libs=-lkernel32 -luser32 -lgdi32 -ldwmapi -lwinmm -lopengl32
 set linker=-Xlinker /NODEFAULTLIB -Xlinker /entry:WinMainCRTStartup -Xlinker /subsystem:windows
 REM set linker=-Xlinker /subsystem:windows
 
 set common=-Wall -Wextra
 
-set conf=-O3 
-rem set conf=-g
+rem set conf=-O3 
+ set conf=-g
 
 if not exist build mkdir build
 
-clang %common% v2.cpp %linker% %conf% -o build\main.exe %libs%
+clang %common% v4\main.cpp %linker% %conf% -o build\main.exe %libs%
 
 if %ERRORLEVEL% EQU 0 (
    call build\main.exe
